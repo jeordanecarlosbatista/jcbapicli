5d29b6c4367f21e7679a14b0d93f0dd128c7d2e1
Jeordane Batista <jeordane.info@unidas.com.br>
2019-01-28 11:11:51 -0200

    Continuação do commit anterior

--- bin/commands/generate/resource.js
+++ bin/commands/generate/resource.js
@@ -5,30 +5,38 @@ const utils = require('../../utils');
 module.exports = {
 
     create: (name) => {
-        const fs = require('fs');
-        const path = require('path');
-
-        const controllerPath = path.join(__dirname, `../../../app/api/controllers/${pluralize.singular(utils.convertPrimeiraLetra(name, false))}Controller.js`);
-        const controllerGenerate = require('../../templates/controller');
-
-        const servicePath = path.join(__dirname, `../../../app/api/services/${pluralize.singular(utils.convertPrimeiraLetra(name, false))}Service.js`);
-        const serviceGenerate = require('../../templates/service');
-
-        const boPath = path.join(__dirname, `../../../app/domain/business/${pluralize.singular(utils.convertPrimeiraLetra(name, false))}BO.js`);
-        const boGenerate = require('../../templates/business');
-
-        const repositoryPath = path.join(__dirname, `../../../app/domain/repository/${pluralize.singular(utils.convertPrimeiraLetra(name, false))}Repository.js`);
-        const repositoryGenerate = require('../../templates/repository');
-
-        if (!fs.existsSync(controllerPath) &&
-            !fs.existsSync(servicePath) &&
-            !fs.existsSync(boPath) &&
-            !fs.existsSync(repositoryPath)) {
-            fs.writeFileSync(controllerPath, controllerGenerate.get(name), 'utf-8');
-            fs.writeFileSync(servicePath, serviceGenerate.get(name), 'utf-8');
-            fs.writeFileSync(boPath, boGenerate.get(name), 'utf-8');
-            fs.writeFileSync(repositoryPath, repositoryGenerate.get(name), 'utf-8');
-            console.log("Arquivos gerados com sucesso!")
+        try {
+            const fs = require('fs');
+            const path = require('path');
+    
+            const controllerPath = path.join(__dirname, `../../../app/api/controllers/${pluralize.singular(utils.convertPrimeiraLetra(name, false))}Controller.js`);
+            const controllerGenerate = require('../../templates/controller');
+    
+            const servicePath = path.join(__dirname, `../../../app/api/services/${pluralize.singular(utils.convertPrimeiraLetra(name, false))}Service.js`);
+            const serviceGenerate = require('../../templates/service');
+    
+            const boPath = path.join(__dirname, `../../../app/domain/business/${pluralize.singular(utils.convertPrimeiraLetra(name, false))}BO.js`);
+            const boGenerate = require('../../templates/business');
+    
+            const repositoryPath = path.join(__dirname, `../../../app/domain/repository/${pluralize.singular(utils.convertPrimeiraLetra(name, false))}Repository.js`);
+            const repositoryGenerate = require('../../templates/repository');
+    
+            if (!fs.existsSync(controllerPath) &&
+                !fs.existsSync(servicePath) &&
+                !fs.existsSync(boPath) &&
+                !fs.existsSync(repositoryPath)) {
+                fs.writeFileSync(controllerPath, controllerGenerate.get(name), 'utf-8');
+                fs.writeFileSync(servicePath, serviceGenerate.get(name), 'utf-8');
+                fs.writeFileSync(boPath, boGenerate.get(name), 'utf-8');
+                fs.writeFileSync(repositoryPath, repositoryGenerate.get(name), 'utf-8');
+                console.log("Arquivos gerados com sucesso!");
+            }
+            else {
+                console.warn("Recurso já existe!");
+            }
+        }
+        catch(err) {
+            console.error(err.message);
         }
     }
 }
